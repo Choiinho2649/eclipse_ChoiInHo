50d3aa3b62c8593d9fe35122986d8c19520a6477
Choiinho2649 <72685866+Choiinho2649@users.noreply.github.com>
2020-11-14 16:02:47 +0900

    PushPrint.java modified on GitHub

--- test/src/test/PushPrint.java
+++ test/src/test/PushPrint.java
@@ -4,7 +4,8 @@ public class PushPrint {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("sophomore : 2�г�");
+		System.out.println("sophomore : 2ÇÐ³â");
+		System.out.println("Jeonbuk Iksan")
 
 	}
 
