7269e96f909b6a667e812ba7cc9469660f7791b9
InHO Choi <dlsgh9230@gmail.com>
2020-11-14 16:06:22 +0900

    added local university school

--- test/src/test/PushPrint.java
+++ test/src/test/PushPrint.java
@@ -5,6 +5,7 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sophomore : 2ÇÐ³â");
+		System.out.println("Wankwang Univ.");
 
 	}
 
