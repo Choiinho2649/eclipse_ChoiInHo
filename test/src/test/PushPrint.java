27128dc9003977422f1d03098a852e2908846350
Choiinho2649 <72685866+Choiinho2649@users.noreply.github.com>
2020-11-14 16:27:03 +0900

    added github fighting!

--- test/src/test/PushPrint.java
+++ test/src/test/PushPrint.java
@@ -5,9 +5,10 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("sophomore : 2�г�");
+		System.out.println("sophomore : 2ÇÐ³â");
 		System.out.println("Wankwang Univ.");
 		System.out.println("Jeonbuk Iksan");
+		System.out.println("fighting!");
 
 
 	}
